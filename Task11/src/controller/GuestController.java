b04e3b5a23c2ffe89ce0e04660ac26ba3cf70d39
Kirill Tarasov <k.tarasov@it-connect.pro>
2025-02-05 10:53:35 +0300

    1

--- Task11/src/controller/GuestController.java
+++
@@ -1,9 +0,0 @@
-package controller;
-
-public interface GuestController {
-    public void listAllGuests();
-    public void listGuestsSortedByName();
-    public void listGuestsSortedByCheckOutDate();
-    public void getTotalGuests();
-    public void getTotalPaymentForGuest(String guestName);  
-} 
