2fadc8818be9855da62c44c98032a79bdd869bad
Kirill1371 <144225525+Kirill1371@users.noreply.github.com>
2025-02-07 19:19:09 +0300

    Task 11 (#11)
    * Task11
    
    * Task11
    
    * fixed
    
    * fixed
    
    * Fixed 3
    
    * Fixed 4
    
    * Fixed 5
    
    ---------
    
    Co-authored-by: Kirill Tarasov <k.tarasov@it-connect.pro>

---
+++ Task11/src/controller/ServiceController.java
@@ -0,0 +1,9 @@
+package controller;
+
+import model.Service;
+
+public interface ServiceController {
+    public void addService(String guestName, Service service);
+    public void listGuestServicesSortedByPrice(String guestName);
+    public void listGuestServicesSortedByDate(String guestName);
+} 
